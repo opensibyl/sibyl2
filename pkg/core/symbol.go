46643334e92bd4f79894f83b03a5303f71a21bd6
williamfzc <178894043@qq.com>
2022-09-26 12:52:04 +0000

    chore: link

--- pkg/core/symbol.go
+++ pkg/core/symbol.go
@@ -10,13 +10,26 @@ type Point struct {
 	Column uint32 `json:"column"`
 }
 
-// Symbol
-// https://github.com/github/semantic/blob/main/docs/examples.md#symbols
+/*
+Symbol
+https://github.com/github/semantic/blob/main/docs/examples.md#symbols
+https://github.com/github/semantic/blob/main/proto/semantic.proto
+
+	enum NodeType {
+		ROOT_SCOPE = 0;
+		JUMP_TO_SCOPE = 1;
+		EXPORTED_SCOPE = 2;
+		DEFINITION = 3;
+		REFERENCE = 4;
+	}
+*/
 type Symbol struct {
 	// value
 	Symbol string `json:"symbol"`
+
 	// range
 	Span Span `json:"span"`
+
 	// type (lang specific
 	// higher analyser will use this field
 	NodeType string `json:"nodeType"`
@@ -24,6 +37,6 @@ type Symbol struct {
 
 type FileSymbol struct {
 	Path     string   `json:"path"`
-	Language string   `json:"language"`
+	Language LangType `json:"language"`
 	Symbols  []Symbol `json:"symbols"`
 }

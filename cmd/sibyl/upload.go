69952eba6b0edf9b2c5f1189633383d1334171fd
williamfzc <178894043@qq.com>
2022-11-17 12:30:33 +0000

    feat: cmd support for upload

---
+++ cmd/sibyl/upload.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
+	"sync"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/spf13/cobra"
+	"github.com/williamfzc/sibyl2"
+	"github.com/williamfzc/sibyl2/pkg/core"
+	"github.com/williamfzc/sibyl2/pkg/extractor"
+	"github.com/williamfzc/sibyl2/pkg/server"
+)
+
+var uploadSrc string
+var uploadLangType string
+var uploadUrl string
+
+func NewUploadCmd() *cobra.Command {
+	uploadCmd := &cobra.Command{
+		Use:    "upload",
+		Short:  "test",
+		Long:   `test`,
+		Hidden: false,
+		Run: func(cmd *cobra.Command, args []string) {
+			uploadSrc, err := filepath.Abs(uploadSrc)
+			if err != nil {
+				panic(err)
+			}
+			repo, err := loadRepo(uploadSrc)
+			if err != nil {
+				panic(err)
+			}
+			head, err := repo.Head()
+			curRepo := filepath.Base(uploadSrc)
+			curRev := head.Hash().String()
+
+			wc := &sibyl2.WorkspaceConfig{
+				RepoId:  curRepo,
+				RevHash: curRev,
+			}
+			f, err := sibyl2.ExtractFunction(uploadSrc, sibyl2.DefaultConfig())
+			if err != nil {
+				panic(err)
+			}
+
+			fullUrl := fmt.Sprintf("%s/api/v1/func", uploadUrl)
+			core.Log.Infof("upload backend: %s", fullUrl)
+			uploadFunctions(fullUrl, wc, f)
+		},
+	}
+	uploadCmd.PersistentFlags().StringVar(&uploadSrc, "src", ".", "src dir path")
+	uploadCmd.PersistentFlags().StringVar(&uploadLangType, "lang", "", "lang type of your source code")
+	uploadCmd.PersistentFlags().StringVar(&uploadUrl, "url", "http://127.0.0.1:9876", "backend url")
+
+	return uploadCmd
+}
+
+func loadRepo(gitDir string) (*git.Repository, error) {
+	repo, err := git.PlainOpen(gitDir)
+	if err != nil {
+		return nil, err
+	}
+	return repo, nil
+}
+
+func uploadFunctions(url string, wc *sibyl2.WorkspaceConfig, f []*extractor.FunctionFileResult) {
+	core.Log.Infof("uploading %v with files %d ...", wc, len(f))
+	var wg sync.WaitGroup
+	wg.Add(len(f))
+	for _, each := range f {
+		unit := &server.FunctionUploadUnit{
+			WorkspaceConfig: wc,
+			FunctionResult:  each,
+		}
+
+		go func() {
+			defer wg.Done()
+			jsonStr, err := json.Marshal(unit)
+			if err != nil {
+				panic(err)
+			}
+			resp, err := http.Post(
+				url,
+				"application/json",
+				bytes.NewBuffer(jsonStr))
+			if err != nil {
+				panic(err)
+			}
+			data, err := io.ReadAll(resp.Body)
+			if resp.StatusCode != http.StatusOK {
+				core.Log.Errorf("upload %s resp: %v", unit.FunctionResult.Path, string(data))
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func init() {
+	uploadCmd := NewUploadCmd()
+	rootCmd.AddCommand(uploadCmd)
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.19
 
 require (
 	github.com/dominikbraun/graph v0.13.0
+	github.com/gin-gonic/gin v1.8.1
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/neo4j/neo4j-go-driver/v5 v5.2.0
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/smacker/go-tree-sitter v0.0.0-20221031025734-03a9c97d8039
 	github.com/spf13/cobra v1.5.0
 	go.uber.org/zap v1.23.0
@@ -14,27 +16,42 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20221026131551-cf6655e29de4 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/cloudflare/circl v1.3.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/xanzy/ssh-agent v0.3.2 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )

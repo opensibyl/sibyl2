bd51f2b5f37ce25a70d9bd64b337e2594ea96680
williamfzc <178894043@qq.com>
2022-11-23 23:59:21 +0800

    refactor(cmd): merge everything into one exe
    in current stage I would rather pay less on delivery

---
+++ cmd/sibyl/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/williamfzc/sibyl2/pkg/server"
+	"github.com/williamfzc/sibyl2/pkg/server/binding"
+)
+
+var backendUri string
+var serverUser string
+var serverPwd string
+
+func NewServerCmd() *cobra.Command {
+	var serverCmd = &cobra.Command{
+		Use:   "server",
+		Short: "sibyl server cmd",
+		Long:  `sibyl server cmd`,
+		Run: func(cmd *cobra.Command, args []string) {
+			config := server.DefaultExecuteConfig()
+			if backendUri != "" {
+				config.DbType = binding.DtNeo4j
+				config.Neo4jUri = backendUri
+			}
+			if serverUser != "" {
+				config.Neo4jUserName = serverUser
+			}
+			if serverPwd != "" {
+				config.Neo4jPassword = serverPwd
+			}
+
+			server.Execute(config)
+		},
+	}
+	serverCmd.PersistentFlags().StringVar(&backendUri, "uri", "", "neo4j backend url")
+	serverCmd.PersistentFlags().StringVar(&serverUser, "user", "", "neo4j user")
+	serverCmd.PersistentFlags().StringVar(&serverPwd, "pwd", "", "neo4j password")
+	return serverCmd
+}
+
+func init() {
+	serverCmd := NewServerCmd()
+	rootCmd.AddCommand(serverCmd)
+}

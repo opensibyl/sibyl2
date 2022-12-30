904aeab49a2acb1f7468fd9b0562342fc8386242
williamfzc <178894043@qq.com>
2022-12-30 13:35:57 +0000

    chore(server/worker): remove unused default value and log

--- pkg/server/worker/upload.go
+++ pkg/server/worker/upload.go
@@ -13,14 +13,14 @@ var funcUnitQueue chan *object.FunctionUploadUnit
 var funcCtxUnitQueue chan *object.FunctionContextUploadUnit
 
 // default neo4j db may be very slow in I/O
-var workerCount = 64
+var workerCount int
 
 // tiny mq, will block request when it is full
 // so make it large enough in your scenario
 // or add a real mq
 // for example,
 // each build for repo which contains 3000 files = 3000 jobs in seconds
-var workerQueueSize = 10240
+var workerQueueSize int
 
 func InitWorker(config object.ExecuteConfig, context context.Context, driver binding.Driver, q queue.Queue) {
 	workerCount = config.WorkerCount
@@ -55,26 +55,22 @@ func startWorker(ctx context.Context, driver binding.Driver) {
 	for {
 		select {
 		case result := <-funcUnitQueue:
-			err := driver.CreateWorkspace(result.WorkspaceConfig, ctx)
-			if err != nil {
-				// allow existed
-				core.Log.Warnf("error when init: %v\n", err)
-			}
+			// failure allowed
+			// todo: waste 1 txn
+			_ = driver.CreateWorkspace(result.WorkspaceConfig, ctx)
 
-			err = driver.CreateFuncFile(result.WorkspaceConfig, result.FunctionResult, ctx)
+			err := driver.CreateFuncFile(result.WorkspaceConfig, result.FunctionResult, ctx)
 			if err != nil {
 				core.Log.Errorf("error when upload: %v\n", err)
 			}
 
 		case result := <-funcCtxUnitQueue:
-			err := driver.CreateWorkspace(result.WorkspaceConfig, ctx)
-			if err != nil {
-				// allow existed
-				core.Log.Warnf("error when init: %v\n", err)
-			}
+			// failure allowed
+			// todo: waste 1 txn
+			_ = driver.CreateWorkspace(result.WorkspaceConfig, ctx)
 
 			for _, each := range result.FunctionContexts {
-				err = driver.CreateFuncContext(result.WorkspaceConfig, each, ctx)
+				err := driver.CreateFuncContext(result.WorkspaceConfig, each, ctx)
 				if err != nil {
 					// deadlock easily happen in neo4j when creating complex edges
 					// append to the queue

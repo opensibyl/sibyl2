4857aad0103d034e91143f06cfe01abe614e4c54
williamfzc <178894043@qq.com>
2023-01-03 23:40:37 +0800

    feat(extractor): python class
    also fix the perf build

--- pkg/extractor/python/extractor_python_class.go
+++ pkg/extractor/python/extractor_python_class.go
@@ -7,17 +7,57 @@ import (
 	"github.com/opensibyl/sibyl2/pkg/extractor/object"
 )
 
+type ClassExtras struct {
+	Decorators []string `json:"decorators"`
+}
+
 func (extractor *Extractor) IsClass(unit *core.Unit) bool {
-	//TODO implement me
+	if unit.Kind == KindPythonClassDefinition {
+		return true
+	}
 	return false
 }
 
 func (extractor *Extractor) ExtractClasses(units []*core.Unit) ([]*object.Clazz, error) {
-	//TODO implement me
-	return nil, errors.New("implement me")
+	var ret []*object.Clazz
+	for _, eachUnit := range units {
+		if !extractor.IsClass(eachUnit) {
+			continue
+		}
+		eachClazz, err := extractor.ExtractClass(eachUnit)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, eachClazz)
+	}
+	return ret, nil
 }
 
 func (extractor *Extractor) ExtractClass(unit *core.Unit) (*object.Clazz, error) {
-	//TODO implement me
-	return nil, errors.New("implement me")
+	clazz := object.NewClazz()
+	clazz.Span = unit.Span
+	clazzName := core.FindFirstByKindInSubsWithBfs(unit, KindPythonIdentifier)
+	if clazzName == nil {
+		return nil, errors.New("class name not found")
+	}
+	clazz.Name = clazzName.Content
+
+	extras := &ClassExtras{}
+	// deco
+	parentUnit := unit.ParentUnit
+	if parentUnit.Kind == KindPythonDecoratedDefinition {
+		decorators := core.FindAllByKindInSubs(parentUnit, KindPythonDecorator)
+		if len(decorators) == 0 {
+			return nil, errors.New("no deco found in " + parentUnit.Content)
+		}
+
+		var decoContents []string
+		for _, each := range decorators {
+			decoContents = append(decoContents, each.Content)
+		}
+		extras.Decorators = decoContents
+	}
+	clazz.Extras = extras
+
+	return clazz, nil
 }

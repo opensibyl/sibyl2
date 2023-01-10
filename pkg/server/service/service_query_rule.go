e39c9a874c5be946794f5178e942b4b59e1d5ea5
williamfzc <178894043@qq.com>
2023-01-10 13:02:30 +0000

    chore(server): split func

--- pkg/server/service/service_query_rule.go
+++ pkg/server/service/service_query_rule.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/opensibyl/sibyl2"
 	"github.com/opensibyl/sibyl2/pkg/server/object"
 )
 
@@ -94,27 +96,32 @@ func HandleFuncCtxQueryWithRule(c *gin.Context) {
 	repo := c.Query("repo")
 	rev := c.Query("rev")
 	rule := c.Query("rule")
+	ret, err := handleFuncCtxQueryWithRule(repo, rev, rule)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	c.JSON(http.StatusOK, ret)
+}
 
+func handleFuncCtxQueryWithRule(repo string, rev string, rule string) ([]*sibyl2.FunctionContext, error) {
 	wc := &object.WorkspaceConfig{
 		RepoId:  repo,
 		RevHash: rev,
 	}
 	if err := wc.Verify(); err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		return
+		return nil, err
 	}
 
 	ruleMap := make(map[string]string)
 	err := json.Unmarshal([]byte(rule), &ruleMap)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		return
+		return nil, fmt.Errorf("failed to unmarshal json: %w", err)
 	}
 
 	functionContexts, err := sharedDriver.ReadFunctionContextsWithRule(wc, ruleMap, sharedContext)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		return
+		return nil, fmt.Errorf("failed to read func ctx: %w", err)
 	}
-	c.JSON(http.StatusOK, functionContexts)
+	return functionContexts, nil
 }
